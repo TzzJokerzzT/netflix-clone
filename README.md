2f3fc4d629480bd92b553d094b5484b877b35fe2
TzzJokerzzT <alexjesus-4856@hotmail.com>
2024-10-11 16:33:55 -0500

    Enjoy a netflix clone app

---
+++ README.md
@@ -0,0 +1,19 @@
+# Netflix Clone
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli).
+
+Clone the project and run
+
+**`npm install`**
+
+### ENJOY ALL THE MOVIES SERIES AND MORE WITH THIS CLONE
+
+#Stack
+
+- Angular
+- Firebase
+- SCSS
+
+#LOGIN AND REGISTER
+
+You can register with you email and password or you can use you Google Account to register
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'netflix-clone-149fd',
+    appId: '1:920450395552:web:6a004f0f9eda0527dc48c5',
+    storageBucket: 'netflix-clone-149fd.appspot.com',
+    apiKey: 'AIzaSyBwtD1SubEgPXPAkg_v8cPeUIzlJ-1WmCc',
+    authDomain: 'netflix-clone-149fd.firebaseapp.com',
+    messagingSenderId: '920450395552',
+  },
+  production: false,
+};
